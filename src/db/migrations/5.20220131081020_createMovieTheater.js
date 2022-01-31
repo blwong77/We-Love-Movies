71ccddec89e581e8fdcfe01350e6a0a0ee1a1d5a
Bradley Wong <blwong77@gmail.com>
2022-01-31 14:06:34 -0800

    made is_showing notnullable

--- src/db/migrations/5.20220131081020_createMovieTheater.js
+++ src/db/migrations/5.20220131081020_createMovieTheater.js
@@ -1,6 +1,6 @@
 exports.up = function (knex) {
   return knex.schema.createTable("movies_theaters", (table) => {
-    table.boolean("is_showing");
+    table.boolean("is_showing").notNullable();
     table.integer("movie_id").unsigned().notNullable();
     table.foreign("movie_id").references("movie_id").inTable("movies");
     table.integer("theater_id").unsigned().notNullable();
