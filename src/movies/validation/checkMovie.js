e386d9e2119400dab219e9b24d42a206db54519c
Bradley Wong <blwong77@gmail.com>
2022-01-31 14:07:59 -0800

    added a validation to check if a movie exists

---
+++ src/movies/validation/checkMovie.js
@@ -0,0 +1,14 @@
+const service = require("../service.movies");
+
+async function checkMovie(req, res, next) {
+  const { movieId } = req.params;
+  const movie = await service.read(movieId);
+
+  if (movie.length !== 0) {
+    res.locals.movie = movie[0];
+    return next();
+  }
+  next({ status: 404, message: "Movie cannot be found." });
+}
+
+module.exports = checkMovie;
