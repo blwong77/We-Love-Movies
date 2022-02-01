55eb08a76978648c2db2599330e3677cf0e0f513
Bradley Wong <blwong77@gmail.com>
2022-02-01 08:00:01 -0800

    imported routers

--- src/movies/router.movies.js
+++ src/movies/router.movies.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const controller = require("./controller.movies");
+const theaterRouter = require("../theaters/router.theaters");
+const reviewRouter = require("../reviews/router.reviews");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
 router.route("/").get(controller.list).all(methodNotAllowed);
@@ -8,6 +10,6 @@ router.route("/:movieId").get(controller.read).all(methodNotAllowed);
 
 router.use("/:movieId/theaters", theaterRouter);
 
-router.use("/:movieId/reviews", reviewRouter)
+router.use("/:movieId/reviews", reviewRouter);
 
 module.exports = router;
