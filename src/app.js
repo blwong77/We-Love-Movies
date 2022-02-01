6975341947e8c5a5aa8bbb3422ce00030b7a4ae0
Bradley Wong <blwong77@gmail.com>
2022-02-01 11:23:20 -0800

    added reviews route

--- src/app.js
+++ src/app.js
@@ -2,4 +2,25 @@ if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
 
+/**
+ * Error Handling Imports
+ */
+const notFound = require("./errors/notFound");
+const errorHandler = require("./errors/errorHandler");
+
+/**
+ * Router Imports
+ */
+const moviesRouter = require("./movies/router.movies");
+const reviewsRouter = require("./reviews/router.reviews")
+
+/**
+ * Pipeline
+ */
+app.use("/movies", moviesRouter);
+app.use("/reviews", reviewsRouter);
+
+app.use(notFound);
+app.use(errorHandler);
+
 module.exports = app;
